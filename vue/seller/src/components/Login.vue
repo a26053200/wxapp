59f07befc744c2bdf72274b4e3a5ee971193d3af
a26053200 <zhengnan262@qq.com>
2018-09-27 23:16:11 +0800

    vue版后台管理系统

---
+++ vue/seller/src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Login11111111111111111111111111111111</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style scoped>
+
+</style>
